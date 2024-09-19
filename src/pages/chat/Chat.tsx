8bc96aa28bd5ee5f85a322b5f4a6786d34fe306f
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2024-09-20 01:43:03 +0700

    .

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -64,7 +64,8 @@ export default function Chat() {
 
          socketRef.current = io(BASE_DOMAIN_API, {
             query: { user_id: info?.user_id },
-            transports: ["websocket"],
+            transports: ["websocket", "polling"],
+            rejectUnauthorized: false,
             secure: isProduction,
          });
 
